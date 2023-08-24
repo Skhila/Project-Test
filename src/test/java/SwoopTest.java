efddee0190561c04380408b3c49ec8c5969821d7
Skhila <bachanaskhiladze@gmail.com>
2023-08-24 04:53:23 +0400

    Corrected some bugs and errors

--- src/test/java/SwoopTest.java
+++ src/test/java/SwoopTest.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -18,55 +21,59 @@ import java.util.Set;
 
 public class SwoopTest {
 //    Setup Driver
-    WebDriver driver;
-    Actions action;
-    JavascriptExecutor js;
-
-//    @BeforeTest
-//    @Parameters("browser")
-//    public void setup(String browser) throws Exception{
-//        if(browser.equalsIgnoreCase("chrome")){
-//            WebDriverManager.chromedriver().setup();
-//            driver = new ChromeDriver();
-//        }else if (browser.equalsIgnoreCase("edge")){
-//            WebDriverManager.edgedriver().setup();
-//            driver = new EdgeDriver();
-//        }else {
-//            throw new Exception("Browser is not correct");
-//        }
-//    }
+    public WebDriver driver;
+    public Actions action;
+    public JavascriptExecutor js;
 
     @BeforeTest
-    public void setup(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setup(String browser) throws Exception{
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        }else if (browser.equalsIgnoreCase("edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }else {
+            throw new Exception("Browser is not correct");
+        }
         action = new Actions(driver);
         js = (JavascriptExecutor) driver;
     }
 
+//    @BeforeTest
+//    public void setup(){
+//        WebDriverManager.edgedriver().setup();
+//        driver = new EdgeDriver();
+//        action = new Actions(driver);
+//        js = (JavascriptExecutor) driver;
+//    }
+
     @Test
     public void swoopTest(){
 //        Navigate to Swoop Website
-        driver.get("https://www.swoop.ge/events/413/lurji-xocho-PG");
+        driver.get("https://www.swoop.ge");
 
 ////        Locate and click on the "კინო" button
-////        WebElement movieButton = driver.findElement(By.xpath("//li[contains(@class,'MoreCategories')][1]/a"));
-//        String movieBtn = "კინო";
-//        WebElement movieButton = driver.findElement(By.xpath("//li[contains(@class,'MoreCategories')]/a[contains(text(), "+movieBtn+")]"));
-//
-//        movieButton.click();
-//
-////        Locate the first movie deal and click buy
-//        WebElement firstMovieElement = driver.findElement(By.xpath("//div[@class='movies-deal'][1]/a"));
-//
-//        action.moveToElement(firstMovieElement).perform();
-//        WebElement buyButton = firstMovieElement.findElement(By.xpath("//div[@class='cinema-hover']/a[2]"));
-//        buyButton.click();
+//        WebElement movieButton = driver.findElement(By.xpath("//li[contains(@class,'MoreCategories')][1]/a"));
+        String movieBtn = "კინო";
+        WebElement movieButton = driver.findElement(By.xpath("//li[contains(@class,'MoreCategories')]/a[contains(text(), "+movieBtn+")]"));
+
+        movieButton.click();
+
+//        Locate the first movie deal and click buy
+        WebElement firstMovieElement = driver.findElement(By.xpath("//div[@class='movies-deal'][1]/a"));
+
+        action.moveToElement(firstMovieElement).perform();
+        WebElement buyButton = firstMovieElement.findElement(By.xpath("//div[@class='cinema-hover']/a[2]"));
+        buyButton.click();
 
 //        Locate East-Point button and click
         String cinemaName = "კავეა ისთ ფოინთი";
         WebElement eastPointElement = driver.findElement(By.xpath("//li/a[text()='" + cinemaName + "']"));
-        eastPointElement.click();
+        js.executeScript("arguments[0].scrollIntoView(true);" +
+                "arguments[0].click()", eastPointElement);
 
 //        Locate the last date and click
         WebElement lastDate = driver.findElement(By.cssSelector("div[aria-hidden=false] div ul li:last-child a"));
@@ -92,16 +99,16 @@ public class SwoopTest {
         driver.switchTo().window(popupWindow);
 
 //        Locate info and check if it is correct
-        String allInfoSelector = "div.right-content div.content-header";
-        new WebDriverWait(driver, 15).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(allInfoSelector)));
-        WebElement allInfo = driver.findElement(By.cssSelector(allInfoSelector));
+        String allInfoSelector = "//*[@id=\"eventForm\"]/div/div[3]/div[1]";
+        new WebDriverWait(driver, 15).until(ExpectedConditions.presenceOfElementLocated(By.xpath(allInfoSelector)));
+        WebElement allInfo = driver.findElement(By.xpath(allInfoSelector));
 
 
 //        Get popup info to check
-        String[] movieInfoAfterArr = allInfo.getText().split("\n");
-        String movieNameAfter = movieInfoAfterArr[0];
-        String cinemaTitleAfter = movieInfoAfterArr[1];
-        String[] seanceDateTimeAfterArr = movieInfoAfterArr[movieInfoAfterArr.length-1].split(" ");
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(allInfo));
+        String movieNameAfter = allInfo.findElement(By.cssSelector("p.movie-title")).getText();
+        String cinemaTitleAfter = allInfo.findElement(By.cssSelector("p:nth-child(2)")).getText();
+        String[] seanceDateTimeAfterArr = allInfo.findElement(By.cssSelector("p:nth-child(3)")).getText().split(" ");
         String seanceDateTimeAfter = seanceDateTimeAfterArr[0] + seanceDateTimeAfterArr[seanceDateTimeAfterArr.length - 1];
 
 //        Check them
@@ -135,9 +142,9 @@ public class SwoopTest {
     }
 
 
-////    Quit Driver
-//    @AfterTest
-//    public void tearDown() {
-//        driver.quit();
-//    }
+//    Quit Driver
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
 }
